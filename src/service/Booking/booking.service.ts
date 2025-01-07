6b7d2b6f360c6bb9b86bedd45c6da8d57cb56919
Bhavesh0078 <bhavesh@angularminds.in>
2025-01-07 17:58:31 +0530

    role base changes

--- src/service/Booking/booking.service.ts
+++ src/service/Booking/booking.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-const API_URL = 'http://localhost:8080/booking';
+const API_URL = 'https://car-rental-backend-lnxb.onrender.com/booking';
 @Injectable({
   providedIn: 'root',
 })
